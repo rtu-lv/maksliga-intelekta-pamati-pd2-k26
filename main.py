d34d7608e010f9a2ea0f132e5bd481cf550de509
Martins Zariņš <hello@zarins.zip>
2026-04-27 15:59:36 +0300

    Added raisin data and visualisation.

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-Building = np.dtype([
-	('compactness', np.float32),
-	('surface_area', np.float32),
-	('wall_area', np.float32),
-	('roof_area', np.float32),
-	('height', np.float32),
-	('orientation', np.uint8),
-	('glazing_area', np.float32),
-	('glazing_distribution', np.float32),
-	('heat_load', np.float32),
-	('cool_load', np.float32),
+Raisin = np.dtype([
+	('Area', np.uint32),
+	('MajorAxisLength', np.float32),
+	('MinorAxisLength', np.float32),
+	('Eccentricity', np.float32),
+	('ConvexArea', np.float32),
+	('Extent', np.float32),
+	('Perimeter', np.float32),
+	('Class', str),
 ])
 
-df = pd.read_csv('./data.csv', dtype=Building)
+df = pd.read_csv('./data.csv', dtype=Raisin)
 
+df.plot.scatter(x='MinorAxisLength', y='Extent')
+plt.show()
 
