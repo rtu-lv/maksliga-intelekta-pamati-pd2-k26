da86ffabe89cf7f4130896f8cb3f9a1648bff708
Martins Zariņš <hello@zarins.zip>
2026-04-27 15:23:32 +0300

    Added dtype and read data.

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+
+import numpy as np
+import pandas as pd
+
+Building = np.dtype([
+	('compactness', np.float32),
+	('surface_area', np.float32),
+	('wall_area', np.float32),
+	('roof_area', np.float32),
+	('height', np.float32),
+	('orientation', np.uint8),
+	('glazing_area', np.float32),
+	('glazing_distribution', np.float32),
+	('heat_load', np.float32),
+	('cool_load', np.float32),
+])
+
+df = pd.read_csv('./data.csv', dtype=Building)
+
 
