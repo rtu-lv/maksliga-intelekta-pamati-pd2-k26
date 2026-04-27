81ea4feeaa5f1afe762735136473164afc31ac07
uentity <uentity1>
2026-04-27 22:29:29 +0300

    part I implementation

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python3
-
 import os
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
 Raisin = np.dtype(
     [
@@ -27,22 +26,31 @@ color_map = {"Kecimen": "blue", "Besni": "orange"}
 
 
 def scatter_plot(df, x_col, y_col, filename, color_map):
-    plt.figure(figsize=(10, 6))
-    for label, color in color_map.items():
-        subset = df[df["Class"] == label]
+    plt.figure(figsize=(6, 4))
+    for (
+        label,
+        color,
+    ) in (
+        color_map.items()
+    ):  # items() returns a view of the dictionary's key-value pairs
+        subset = df[
+            df["Class"] == label
+        ]  # filter the dataframe by class label all at once (broadcasting)
         plt.scatter(
-            subset[x_col],
-            subset[y_col],
+            subset[x_col],  # take the filtered x column
+            subset[y_col],  # take the filtered y column
             c=color,
             label=label,
             alpha=0.6,
             edgecolors="w",
-            s=60,
+            s=30,
         )
-    plt.title(f"Raisin Classification: {x_col} vs {y_col}", fontsize=14)
-    plt.xlabel(x_col, fontsize=12)
-    plt.ylabel(y_col, fontsize=12)
-    plt.legend(title="Raisin Variety")
+    plt.title(f"{x_col} vs {y_col}", fontsize=11)
+    plt.xlabel(x_col, fontsize=8)
+    plt.ylabel(y_col, fontsize=8)
+    plt.xticks(fontsize=6)
+    plt.yticks(fontsize=6)
+    plt.legend(title="Raisin Type", fontsize=8, title_fontsize=8)
     plt.grid(True, linestyle="--", alpha=0.5)
     plt.savefig(filename, dpi=300)
 
@@ -57,3 +65,56 @@ scatter_plot(
 scatter_plot(
     df, "Area", "Extent", "graphs/scatterplots/raisin_scatterplot_2.png", color_map
 )
+
+os.makedirs("graphs/histograms", exist_ok=True)
+
+
+def histogram(df, col, filename, color_map):
+    plt.figure(figsize=(6, 4))
+
+    for label, color in color_map.items():
+        subset = df[df["Class"] == label]
+        plt.hist(
+            subset[col],  # take the filtered column
+            bins=30,
+            color=color,
+            alpha=0.6,
+            label=label,
+        )
+    plt.title(f"Distribution of {col}", fontsize=11)
+    plt.xlabel(col, fontsize=8)
+    plt.ylabel("Frequency", fontsize=8)
+    plt.xticks(fontsize=6)
+    plt.yticks(fontsize=6)
+    plt.legend(title="Raisin Type", fontsize=8, title_fontsize=8)
+    plt.grid(True, linestyle="--", alpha=0.5)
+    plt.savefig(filename, dpi=300)
+
+
+histogram(df, "MajorAxisLength", "graphs/histograms/raisin_histogram_1.png", color_map)
+histogram(df, "MinorAxisLength", "graphs/histograms/raisin_histogram_2.png", color_map)
+
+
+def box_plot(df, col, filename):
+    plt.figure(figsize=(8, 6))
+    sns.boxplot(x="Class", y=col, data=df)
+    plt.title(f"Raisin Distribution: {col}", fontsize=14)
+    plt.savefig(filename, dpi=300)
+
+
+def violin_plot(df, col, filename):
+    plt.figure(figsize=(10, 6))
+    sns.violinplot(x="Class", y=col, data=df)
+    plt.title(f"Raisin Distribution: {col}", fontsize=14)
+    plt.savefig(filename, dpi=300)
+
+
+os.makedirs("graphs/distributions", exist_ok=True)
+
+box_plot(df, "ConvexArea", "graphs/distributions/raisin_boxplot.png")
+violin_plot(df, "Eccentricity", "graphs/distributions/raisin_violinplot.png")
+
+os.makedirs("statistics", exist_ok=True)
+
+stats = df.groupby("Class").agg(["mean", "var"])
+stats.to_excel("statistics/statistics.xlsx")
