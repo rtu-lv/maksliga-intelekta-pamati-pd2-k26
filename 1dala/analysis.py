98bf48df580abfc8d7bc41a01c64ff14f207264a
Martins Zariņš <hello@zarins.zip>
2026-05-07 12:47:42 +0300

    Fixed missing plot legend.
    Also removed useless code. Actually just undid the whole previous
    change.

--- 1dala/analysis.py
+++ 1dala/analysis.py
@@ -19,20 +19,15 @@ stats = df.groupby('Class').agg(['mean', 'var'])
 print(stats.T)
 
 
-color_map = {
-    "Kecimen": "red",
-    "Besni": "blue"
-}
-
 for x, y in [('MajorAxisLength', 'MinorAxisLength'), ('Area', 'Perimeter'), ('Area', 'Extent')]:
-	ax = df.plot.scatter(x, y, c=df["Class"].map(color_map), alpha=0.6)
+	df.plot.scatter(x, y, c='Class', alpha=0.6)
 
 for measure in ['MajorAxisLength', 'MinorAxisLength', 'Area']:
-	ax = df.pivot(columns='Class', values=measure) \
+	df.pivot(columns='Class', values=measure) \
 	  .plot.hist(bins=30, xlabel=measure, alpha=0.6)
-	ax.set_xlabel(measure)
 
 for measure in ['ConvexArea', 'Eccentricity']:
-	ax = df.plot.box(by='Class', column=measure)
+	df.plot.box(by='Class', column=measure)
+
 plt.show()
 
